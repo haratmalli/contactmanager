232e8fcc041dc5850b349f91424ddd269429e453
Harat Malli <haratmalli@gmail.com>
2018-11-26 16:40:54 +0530

    Updates

---
+++ precache-manifest.74db21f4fa8173d24dff0959e828b16f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "b02c36aa82f2cead827c",
+    "url": "/contactmanager/static/js/main.b02c36aa.chunk.js"
+  },
+  {
+    "revision": "21e09122fe8556452669",
+    "url": "/contactmanager/static/js/1.21e09122.chunk.js"
+  },
+  {
+    "revision": "21e09122fe8556452669",
+    "url": "/contactmanager/static/css/1.9a79eead.chunk.css"
+  },
+  {
+    "revision": "632fd8c32eebe59089eac5ce2c85fd28",
+    "url": "/contactmanager/index.html"
+  }
+];
